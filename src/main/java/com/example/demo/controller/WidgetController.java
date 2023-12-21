a05fcb8aa000ba58986662c0872670b4c511413f
itonse <govl3dnjs@gmail.com>
2023-12-21 10:24:44 +0900

    feat : OrderService 추가
    - 주문번호의 최종 결제금액이 amount와 일치하는지 체크하는 로직 추가

--- src/main/java/com/example/demo/controller/WidgetController.java
+++ src/main/java/com/example/demo/controller/WidgetController.java
@@ -1,20 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -23,7 +28,13 @@ import java.util.Base64;
 @Controller
 public class WidgetController {
 
+    @Value("${api.key}")
+    private String API_KEY;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private OrderService orderService;
 
     @RequestMapping(value = "/confirm")
     public ResponseEntity<JSONObject> confirmPayment(@RequestBody String jsonBody) throws Exception {
@@ -36,11 +47,16 @@ public class WidgetController {
             // 클라이언트에서 받은 JSON 요청 바디입니다.
             JSONObject requestData = (JSONObject) parser.parse(jsonBody);
             paymentKey = (String) requestData.get("paymentKey");
-            orderId = (String) requestData.get("orderId");
-            amount = (String) requestData.get("amount");
+            orderId = (String) requestData.get("orderId");    // 중요
+            amount = (String) requestData.get("amount");      // 중요
         } catch (ParseException e) {
             throw new RuntimeException(e);
         };
+
+        // 체크
+        orderService.checkAmount(orderId, amount);
+
+
         JSONObject obj = new JSONObject();
         obj.put("orderId", orderId);
         obj.put("amount", amount);
@@ -48,7 +64,7 @@ public class WidgetController {
 
         // TODO: 개발자센터에 로그인해서 내 결제위젯 연동 키 > 시크릿 키를 입력하세요. 시크릿 키는 외부에 공개되면 안돼요.
         // @docs https://docs.tosspayments.com/reference/using-api/api-keys
-        String apiKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
+        String apiKey = API_KEY;
 
         // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
         // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
@@ -73,6 +89,14 @@ public class WidgetController {
 
         int code = connection.getResponseCode();
         boolean isSuccess = code == 200 ? true : false;
+
+        // 결제 승인이 완료
+        if ( isSuccess ) {
+            orderService.setPaymentComplete(orderId);
+        }
+        else {
+            throw new RuntimeException("결제 승인 실패");
+        }
 
         InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
 
